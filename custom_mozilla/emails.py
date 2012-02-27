1689b8b9c7c8ed59941a0f245e26cdd4a9744208
Andy McKay <amckay@mozilla.com>
2012-02-27 15:40:37 -0800

    redirect to amo-tracebacks and capture marketplace

--- custom_mozilla/emails.py
+++ custom_mozilla/emails.py
@@ -5,19 +5,22 @@ from django.contrib.auth.models import User
 from projects.models import Project
 from error.signals import error_created
 
-amo_users = ['amckay@mozilla.com'] 
-amo_domains = ['addons-dev.allizom.org', 'addons.mozilla.org', 'addons.allizom.org',
-               'apps-preview-dev.allizom.org', 'apps-preview.mozilla.org', 'apps-preview.allizom.org']
+amo_users = ['amo-tracebacks@mozilla.org']
+amo_domains = ['addons-dev.allizom.org', 'addons.allizom.org', 'addons.mozilla.org',
+               'apps-preview-dev.allizom.org', 'apps-preview.allizom.org', 'apps-preview.mozilla.org',
+               'marketplace-dev.allizom.org', 'marketplace.allizom.org', 'marketplace.mozilla.org']
+
 
 def amo_notification(instance, **kw):
     """ Given an error see if we need to send a notification """
-    log("Firing signal: default_notification")
+    log('Firing signal: default_notification')
 
     user = User.objects.get(email__in=amo_users)
     if instance.domain in amo_domains:
-    	notification = Notification()
-    	notification.notifier = instance
-    	notification.save()
-	notification.user.add(user)
-    
-error_created.connect(amo_notification, dispatch_uid="amo_notification")
+        notification = Notification()
+        notification.notifier = instance
+        notification.save()
+    notification.user.add(user)
+
+
+error_created.connect(amo_notification, dispatch_uid='amo_notification')
